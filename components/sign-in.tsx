a57957ff40a3e6b5dc33aad380b1052c2c704683
Andrew David Kay <andrewdavidkay@icloud.com>
2025-08-12 16:23:34 -0400

    fix sign in button background bleed on ios

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -10,7 +10,14 @@ export default function SignIn() {
     >
       <button
         type="submit"
-        className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800 transition-colors"
+        className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800 transition-colors -webkit-appearance: none appearance-none border-0 outline-none"
+        style={{
+          WebkitAppearance: "none",
+          appearance: "none",
+          border: "none",
+          outline: "none",
+          backgroundClip: "padding-box",
+        }}
       >
         Sign in with X
       </button>
